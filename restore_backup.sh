878555e2ed0fee84a3c809a384d538c39d5699ca
Nomad <nomadapp.x@gmail.com>
2024-03-24 17:08:11 +0530

    restore script

---
+++ restore_backup.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Function to execute command and log output
+execute_command() {
+    echo "Executing: $1"
+    if ! eval "$1"; then
+        echo "Error: Command failed: $1"
+        exit 1
+    else
+        echo "Success: Command executed successfully: $1"
+    fi
+}
+
+
+find_var() {
+    local allowed_origin_input="$1"
+    local found=false
+    local arg
+    local allowed_origin_value
+
+    # Iterate through all positional arguments
+    for arg in "${@:2}"; do
+        # Check if the argument is "ALLOWED_ORIGIN"
+        if [[ "$arg" == "$allowed_origin_input="* ]]; then
+            # Extract the value of ALLOWED_ORIGIN (removing the prefix)
+            allowed_origin_value="${arg#$allowed_origin_input=}"
+            found=true
+            break  # Exit loop after finding ALLOWED_ORIGIN
+        fi
+    done
+
+    # If ALLOWED_ORIGIN was not found, exit the script
+    if ! $found; then
+        echo "$allowed_origin_input not found."
+        exit 1
+    fi
+
+    echo "$allowed_origin_value"
+}
+
+# Change directory into nodeserver
+cd appwrite-deployment || exit 1
+
+# Download mariadb and influxdb backup files
+BACKUP_TIMESTAMP=$(find_var "BACKUP_TIMESTAMP" "$@")
+BACKBLAZE_B2_KEY_ID=$(find_var "BACKBLAZE_B2_KEY_ID" "$@")
+BACKBLAZE_B2_KEY=$(find_var "BACKBLAZE_B2_KEY" "$@")
+BUCKET_NAME=$(find_var "BUCKET_NAME" "$@")
+execute_command 'docker run   --rm   -v $PWD:/b2   mtronix/b2-cli:0.0.1   bash -c   "b2 authorize-account $BACKBLAZE_B2_KEY_ID $BACKBLAZE_B2_KEY && b2 download-file-by-name $BUCKET_NAME dump_$BACKUP_TIMESTAMP.sql ./dump_$BACKUP_TIMESTAMP.sql && b2 download-file-by-name $BUCKET_NAME influxdb-backup_$BACKUP_TIMESTAMP.tar.gz ./influxdb-backup_$BACKUP_TIMESTAMP.tar.gz"'
+
+
+
+# Restore from dump.sql file
+MARIADB_USER=$(find_var "MARIADB_USER" "$@")
+MARIADB_PASSWORD=$(find_var "MARIADB_PASSWORD" "$@")
+execute_command 'docker compose exec -T mariadb sh -c "exec mysql -u\"$MARIADB_USER\" -p\"$MARIADB_PASSWORD\"" < dump_$BACKUP_TIMESTAMP.sql'
+
+
+# extract influxdb_backup.tar.gz
+
+# copy influxdb_backup to influxdb_container
+
+# restore influxdb
+execute_command 'docker compose exec influxdb   sh -c   "influxd restore --portable /storage/influxdb-backup_$current_timestamp"'
+
+
+# remove dump file
+execute_command "rm dump_$BACKUP_TIMESTAMP.sql && rm influxdb-backup_$BACKUP_TIMESTAMP.tar.gz"
